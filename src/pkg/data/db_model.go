c9a69e0d81e292aaa32b8979d4f59800dab90d6f
pelusa-v <razor0164@gmail.com>
2024-04-24 23:32:09 -0500

    Map embedded structs in models using reflection

--- src/pkg/data/db_model.go
+++ src/pkg/data/db_model.go
@@ -42,53 +42,6 @@ func (m *DbModel) GetObject(pk string) interface{} {
 	return concreteObject
 }
 
-// func (m *DbModel) ListObjects() []DbObjectInstance {
-// 	objectsType := reflect.SliceOf(m.modelType)
-// 	concreteDbObjects := reflect.New(objectsType).Interface()
-
-// 	concreteStruct := reflect.New(m.modelType).Interface()
-// 	query := m.db.Model(concreteStruct)
-// 	query.Find(concreteDbObjects)
-
-// 	concreteDbObjectsValue := reflect.ValueOf(concreteDbObjects).Elem()
-// 	// resultSlice := make([]interface{}, concreteDbObjectsValue.Len()) // To delete
-
-// 	objects := make([]DbObjectInstance, concreteDbObjectsValue.Len())
-
-// 	for i := 0; i < concreteDbObjectsValue.Len(); i++ {
-// 		dbObject := concreteDbObjectsValue.Index(i)
-// 		object := DbObjectInstance{}
-// 		var objectProperties []reflect.StructField
-// 		var objectPropertiesValues []reflect.Value
-
-// for i := 0; i < dbObject.NumField(); i++ {
-// 	propertyValue := dbObject.Field(i)
-// 	property := dbObject.Type().Field(i)
-
-// 	objectPropertiesValues = append(objectPropertiesValues, propertyValue)
-// 	objectProperties = append(objectProperties, property)
-
-// 	fmt.Println("---------------------------")
-// 	fmt.Printf("%v :\n", dbObject.Type().Field(i).Name)
-// 	fmt.Printf("%v :\n", dbObject.Type().Field(i).Type)
-// 	fmt.Printf("%v :\n", dbObject.Type().Field(i).Tag)
-// 	fmt.Printf("%v :\n", dbObject.Type().Field(i))
-// 	fmt.Println(dbObject.Field(i))
-// }
-
-// 		object.Fields = objectProperties
-// 		object.FieldsValues = objectPropertiesValues
-// 		objects = append(objects, object)
-// 		// resultSlice[i] = concreteDbObjectsValue.Index(i).Interface() // To delete
-// 	}
-
-// 	return objects
-// }
-
-func (m *DbModel) GetModelFields() []reflect.StructField {
-	var fields []reflect.StructField
-	for i := 0; i < m.modelType.NumField(); i++ {
-		fields = append(fields, m.modelType.Field(i))
-	}
-	return fields
+func FieldHasEmbeddedStructs(f reflect.StructField) bool {
+	return f.Anonymous || f.Type == reflect.TypeOf(gorm.DeletedAt{})
 }
--- src/pkg/data/templates_data.go
+++ src/pkg/data/templates_data.go
@@ -34,10 +34,6 @@ func (item *Model) DetailURL() string {
 	return fmt.Sprintf("/admin/%s", item.Name)
 }
 
-// func (item *ModelObject) DetailObjectDetailURL() string {
-// 	return fmt.Sprintf("/admin/%s/%s", item.Name)
-// }
-
 func GetHomePageData(modelTypes *[]reflect.Type) HomePageData {
 	data := HomePageData{}
 	modelsList := make([]Model, len(*modelTypes))
@@ -50,12 +46,13 @@ func GetHomePageData(modelTypes *[]reflect.Type) HomePageData {
 }
 
 func GetModelDetailPageData(model DbModel) ModelDetailPageData {
-	data := ModelDetailPageData{Model: model.modelType.Name(), ModelObjectsFields: model.GetModelFields()}
+	modelFields := GetObjectFields(model.modelType)
+	data := ModelDetailPageData{Model: model.modelType.Name(), ModelObjectsFields: modelFields}
 
 	var modelObjects []ModelObject
 	objects := model.ListObjects()
 	for _, o := range objects {
-		modelObject := GetObjectFields(o)
+		modelObject := MapModelObject(o)
 		modelObjects = append(modelObjects, modelObject)
 	}
 	data.ModelObjects = modelObjects
@@ -65,31 +62,29 @@ func GetModelDetailPageData(model DbModel) ModelDetailPageData {
 
 func GetModelObjectDetailPageData(model DbModel, pk string) ModelObjectDetailPageData {
 	object := model.GetObject(pk)
-	data := ModelObjectDetailPageData{Model: model.modelType.Name(), ModelObject: GetObjectFields(object)}
+	data := ModelObjectDetailPageData{Model: model.modelType.Name(), ModelObject: MapModelObject(object)}
 	return data
 }
 
-func GetObjectFields(o interface{}) ModelObject {
+func MapModelObject(o interface{}) ModelObject {
 	objectValue := reflect.ValueOf(o)
 	if objectValue.Kind() == reflect.Ptr {
 		objectValue = objectValue.Elem()
 	}
 
-	var objectFields []reflect.StructField
-	var objectFieldsValues []reflect.Value
+	objectFields := GetObjectFields(objectValue.Type())
+	objectFieldsValues := GetObjectFieldsValues(objectValue)
 
-	for i := 0; i < objectValue.NumField(); i++ {
-		objectFields = append(objectFields, objectValue.Type().Field(i))
-		objectFieldsValues = append(objectFieldsValues, objectValue.Field(i))
+	fmt.Printf("objectFields: %d\n", len(objectFields))
+	fmt.Printf("objectFieldsValues: %d\n", len(objectFieldsValues))
+
+	for i := 0; i < len(objectFields); i++ {
+		fmt.Printf("%v - %v (%v)\n", objectFields[i].Name, objectFields[i].Type, objectFields[i].Tag)
 	}
+	fmt.Println("------------------------------------")
 
-	fmt.Println("---------------------------")
-	for i := 0; i < objectValue.NumField(); i++ {
-		fmt.Printf("NAME: %v :\n", objectValue.Type().Field(i).Name)
-		fmt.Printf("TYPE: %v :\n", objectValue.Type().Field(i).Type)
-		fmt.Printf("TAG: %v :\n", objectValue.Type().Field(i).Tag)
-		fmt.Printf("%v :\n", objectValue.Type().Field(i))
-		fmt.Println(objectValue.Field(i))
+	for i := 0; i < len(objectFieldsValues); i++ {
+		fmt.Printf("%v\n", objectFieldsValues[i])
 	}
 
 	return ModelObject{
@@ -98,6 +93,68 @@ func GetObjectFields(o interface{}) ModelObject {
 	}
 }
 
-// func GetPkField(o interface{}) ModelObject {
+func GetObjectFieldsValues(objectValue reflect.Value) []reflect.Value {
+	if objectValue.Kind() == reflect.Ptr {
+		objectValue = objectValue.Elem()
+	}
 
+	var objectFieldsValues []reflect.Value
+
+	objectType := objectValue.Type()
+	for i := 0; i < objectValue.NumField(); i++ {
+		fieldType := objectType.Field(i)
+		fieldValue := objectValue.Field(i)
+
+		if FieldHasEmbeddedStructs(fieldType) {
+			embeddedFieldsValues := GetObjectFieldsValues(fieldValue)
+			objectFieldsValues = append(objectFieldsValues, embeddedFieldsValues...)
+		} else {
+			objectFieldsValues = append(objectFieldsValues, fieldValue)
+		}
+	}
+
+	return objectFieldsValues
+}
+
+func GetObjectFields(objectType reflect.Type) []reflect.StructField {
+	var objectFields []reflect.StructField
+
+	for i := 0; i < objectType.NumField(); i++ {
+		fieldType := objectType.Field(i)
+
+		if FieldHasEmbeddedStructs(fieldType) {
+			embeddedFields := GetObjectFields(fieldType.Type)
+			objectFields = append(objectFields, embeddedFields...)
+		} else {
+			objectFields = append(objectFields, fieldType)
+		}
+	}
+
+	return objectFields
+}
+
+// func GetFieldsAndValues(objectValue reflect.Value) ([]reflect.StructField, []reflect.Value) {
+// 	if objectValue.Kind() == reflect.Ptr {
+// 		objectValue = objectValue.Elem()
+// 	}
+
+// 	var objectFields []reflect.StructField
+// 	var objectFieldsValues []reflect.Value
+
+// 	objectType := objectValue.Type()
+// 	for i := 0; i < objectValue.NumField(); i++ {
+// 		fieldType := objectType.Field(i)
+// 		fieldValue := objectValue.Field(i)
+
+// 		if FieldHasEmbeddedStructs(fieldType) {
+// 			embeddedFields, embeddedFieldsValues := GetFieldsAndValues(fieldValue)
+// 			objectFields = append(objectFields, embeddedFields...)
+// 			objectFieldsValues = append(objectFieldsValues, embeddedFieldsValues...)
+// 		} else {
+// 			objectFields = append(objectFields, fieldType)
+// 			objectFieldsValues = append(objectFieldsValues, fieldValue)
+// 		}
+// 	}
+
+// 	return objectFields, objectFieldsValues
 // }
