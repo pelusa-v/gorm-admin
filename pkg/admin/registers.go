7a078f26ad1e64ee1b6de90c474b7c6a5c34cdc1
pelusa-v <razor0164@gmail.com>
2024-07-25 01:48:46 -0500

    Template extensions updated

--- pkg/admin/registers.go
+++ pkg/admin/registers.go
@@ -9,10 +9,10 @@ import (
 )
 
 func (admin *Admin) registerHomePage() {
-	templates := admin.template("templates/*.html")
+	templates := admin.template("templates/*.gohtml")
 	templateManager := data.NewTemplateManager(&admin.Name, &admin.Models)
 
-	admin.Handler.RegisterSimplePage(templates, "home.html", "/admin", func() any {
+	admin.Handler.RegisterSimplePage(templates, "home.gohtml", "/admin", func() any {
 		return templateManager.GetHomePageData()
 	})
 
@@ -22,33 +22,33 @@ func (admin *Admin) registerHomePage() {
 
 func (admin *Admin) registerModelDetailPage(modelType reflect.Type) {
 	dbModel := data.NewDbModel(modelType, admin.GormDB)
-	templates := admin.template("templates/*.html")
+	templates := admin.template("templates/*.gohtml")
 	templateManager := data.NewTemplateManager(&admin.Name, &admin.Models)
 	modelDetailPageRoute := fmt.Sprintf("/admin/%s", modelType.Name())
 
-	admin.Handler.RegisterSimplePage(templates, "ModelDetail.html", modelDetailPageRoute, func() any {
+	admin.Handler.RegisterSimplePage(templates, "ModelDetail.gohtml", modelDetailPageRoute, func() any {
 		return templateManager.GetModelDetailPageData(*dbModel)
 	})
 }
 
 func (admin *Admin) registerModelObjectDetailPage(modelType reflect.Type) {
 	dbModel := data.NewDbModel(modelType, admin.GormDB)
-	templates := admin.template("templates/*.html")
+	templates := admin.template("templates/*.gohtml")
 	templateManager := data.NewTemplateManager(&admin.Name, &admin.Models)
 	modelObjectDetailPageRoute := fmt.Sprintf("/admin/%s/:pk", modelType.Name())
 
-	admin.Handler.RegisterPkPage(templates, "ModelObjectDetail.html", modelObjectDetailPageRoute, func(pk string) any {
+	admin.Handler.RegisterPkPage(templates, "ModelObjectDetail.gohtml", modelObjectDetailPageRoute, func(pk string) any {
 		return templateManager.GetModelObjectDetailPageData(*dbModel, pk)
 	})
 }
 
 func (admin *Admin) registerModelObjectCreatePage(modelType reflect.Type) {
 	dbModel := data.NewDbModel(modelType, admin.GormDB)
-	templates := admin.template("templates/*.html")
+	templates := admin.template("templates/*.gohtml")
 	templateManager := data.NewTemplateManager(&admin.Name, &admin.Models)
 	modelObjectCreatePageRoute := fmt.Sprintf("/admin/%s/actions/create", modelType.Name())
 
-	admin.Handler.RegisterSimplePage(templates, "ModelObjectCreate.html", modelObjectCreatePageRoute, func() any {
+	admin.Handler.RegisterSimplePage(templates, "ModelObjectCreate.gohtml", modelObjectCreatePageRoute, func() any {
 		return templateManager.GetModelObjectCreatePageData(*dbModel)
 	})
 }
@@ -63,11 +63,11 @@ func (admin *Admin) registerModelObjectCreateEndpoint(modelType reflect.Type) {
 
 func (admin *Admin) registerModelObjectUpdatePage(modelType reflect.Type) {
 	dbModel := data.NewDbModel(modelType, admin.GormDB)
-	templates := admin.template("templates/*.html")
+	templates := admin.template("templates/*.gohtml")
 	templateManager := data.NewTemplateManager(&admin.Name, &admin.Models)
 	modelObjectUpdatePageRoute := fmt.Sprintf("/admin/%s/actions/update/:pk", modelType.Name())
 
-	admin.Handler.RegisterPkPage(templates, "ModelObjectCreate.html", modelObjectUpdatePageRoute, func(pk string) any {
+	admin.Handler.RegisterPkPage(templates, "ModelObjectCreate.gohtml", modelObjectUpdatePageRoute, func(pk string) any {
 		return templateManager.GetModelObjectUpdatePageData(*dbModel, pk)
 	})
 }
